5ac40ebf98124cdecb9cb5276c41c579ee5f111b
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-17 14:33:53 +0530

    Create assets.js

---
+++ assets.js
@@ -0,0 +1,24 @@
+const characters = [
+  {
+    name: "🔥 Tanjiro Kamado",
+    rarity: "Rare",
+    image: "https://i.pinimg.com/736x/29/74/86/2974861890aced868e7402a78350fe0c.jpg",
+  },
+  {
+    name: "⚡ Zenitsu Agatsuma",
+    rarity: "Epic",
+    image: "https://i.pinimg.com/736x/5d/e5/b5/5de5b5553b3c1a5d0d2722cfcb93a6c5.jpg",
+  },
+  {
+    name: "🐗 Inosuke Hashibira",
+    rarity: "Common",
+    image: "https://i.pinimg.com/736x/4f/e4/c7/4fe4c7d93165edd30b361d225250d631.jpg",
+  },
+  {
+    name: "🌊 Giyu Tomioka",
+    rarity: "Legendary",
+    image: "https://i.pinimg.com/1200x/39/47/93/394793954cc6f6734098cadb7f8b2496.jpg",
+  },
+];
+
+module.exports = characters;
