7d76f0504ef44b0b28a38dccc99f491380c1bfbf
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-18 16:25:03 +0530

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,22 +1,18 @@
-// Remove the requires and bot initialization from here, 
-// because index.js already handles that!
+module.exports = (bot) => {
+  bot.onText(/^\/start$/, async (msg) => {
+    const chatId = msg.chat.id;
 
-module.exports = (bot) => { 
-  bot.onText(/\/start/, async (msg) => { 
-    const chatId = msg.chat.id; 
-   
-    try { 
-      await bot.sendPhoto( 
-        chatId, 
-        "https://kommodo.ai/i/ip5xqtnqGqXGpuQxOpkZ", 
-        { 
-          // Added quotation marks around the string!
-          caption: "⚔️ WELCOME TO DEMON SLAYER BOT ⚔️", 
-        } 
-      ); 
-    } catch (err) { 
-      console.log(err); 
-      bot.sendMessage(chatId, "Error aa gaya start image me 😓"); 
-    } 
-  }); 
+    try {
+      await bot.sendPhoto(
+        chatId,
+        "https://kommodo.ai/i/ip5xqtnqGqXGpuQxOpkZ",
+        {
+          caption: "⚔️ WELCOME TO DEMON SLAYER BOT ⚔️",
+        }
+      );
+    } catch (err) {
+      console.log(err);
+      bot.sendMessage(chatId, "Error aa gaya start image me 😓");
+    }
+  });
 };
