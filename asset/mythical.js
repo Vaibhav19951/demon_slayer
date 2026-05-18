7f7a449fb2d123d60515017e1674658882314b36
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-18 16:15:03 +0530

    Create mythical.js

---
+++ asset/mythical.js
@@ -0,0 +1,137 @@
+module.exports = [
+  {
+    id: "tanjiro_sun_breathing",
+    name: "Tanjiro Kamado [Sun Breathing Limited]",
+    rarity: "Mythical Limited",
+    power: 7000,
+    cost: 5,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/71/0d/48/710d48e1f4e127760980576724bb10fa.jpg"
+  },
+  {
+    id: "nezuko_demon_queen",
+    name: "Nezuko Kamado [Demon Queen Limited]",
+    rarity: "Mythical Limited",
+    power: 6800,
+    cost: 5,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/86/c5/80/86c5801b3eb25ca6e4ac2bfc2e6fd23d.jpg"
+  },
+  {
+    id: "zenitsu_thunder_god",
+    name: "Zenitsu Agatsuma [Thunder God Limited]",
+    rarity: "Mythical Limited",
+    power: 6900,
+    cost: 5,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/50/91/9c/50919cf34126eaf521d011bb4d05b84d.jpg"
+  },
+  {
+    id: "inosuke_beast_king",
+    name: "Inosuke Hashibira [Beast King Limited]",
+    rarity: "Mythical Limited",
+    power: 6700,
+    cost: 4,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/bc/f7/67/bcf7673443d02979a1f134d3cf59cc65.jpg"
+  },
+  {
+    id: "rengoku_flame_emperor",
+    name: "Kyojuro Rengoku [Flame Emperor Limited]",
+    rarity: "Mythical Limited",
+    power: 7200,
+    cost: 6,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/7b/39/3b/7b393b1bbc7dbc4c73a30ba80d29e89c.jpg"
+  },
+  {
+    id: "giyuu_dead_calm",
+    name: "Giyuu Tomioka [Dead Calm Limited]",
+    rarity: "Mythical Limited",
+    power: 7100,
+    cost: 6,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/30/b3/e9/30b3e97d13dff5e879828b05ab27c9b5.jpg"
+  },
+  {
+    id: "shinobu_poison_empress",
+    name: "Shinobu Kocho [Poison Empress Limited]",
+    rarity: "Mythical Limited",
+    power: 7000,
+    cost: 6,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/4d/ec/38/4dec38f75bb4a18367df3837c6f78086.jpg"
+  },
+  {
+    id: "tengen_sound_master",
+    name: "Tengen Uzui [Sound Master Limited]",
+    rarity: "Mythical Limited",
+    power: 7300,
+    cost: 6,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/be/4e/bf/be4ebf17b3c92d0a262c50fd60748ec6.jpg"
+  },
+  {
+    id: "muichiro_mist_prodigy",
+    name: "Muichiro Tokito [Mist Prodigy Limited]",
+    rarity: "Mythical Limited",
+    power: 7400,
+    cost: 6,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/be/84/47/be8447c25a2ed67abc7dde58206b2eb8.jpg"
+  },
+  {
+    id: "mitsuri_love_blade",
+    name: "Mitsuri Kanroji [Love Blade Limited]",
+    rarity: "Mythical Limited",
+    power: 7100,
+    cost: 6,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/56/c0/94/56c094b0cabad5d895b100a9f9967ffc.jpg"
+  },
+  {
+    id: "obanai_serpent_curse",
+    name: "Obanai Iguro [Serpent Curse Limited]",
+    rarity: "Mythical Limited",
+    power: 7250,
+    cost: 6,
+    type: "mythical",
+    image: "https://i.pinimg.com/1200x/75/3d/d6/753dd68ff074d8e89275b2dfe30b9c82.jpg"
+  },
+  {
+    id: "sanemi_wind_fury",
+    name: "Sanemi Shinazugawa [Wind Fury Limited]",
+    rarity: "Mythical Limited",
+    power: 7600,
+    cost: 7,
+    type: "mythical",
+    image: "https://i.pinimg.com/1200x/cd/e6/a9/cde6a94862cd3979cd3ae147986c4d8e.jpg"
+  },
+  {
+    id: "akaza_blood_battle",
+    name: "Akaza [Blood Battle Limited]",
+    rarity: "Mythical Limited",
+    power: 7800,
+    cost: 7,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/46/23/c0/4623c021439e3c6c4913420581f9fddc.jpg"
+  },
+  {
+    id: "kokushibo_moon_king",
+    name: "Kokushibo [Moon King Limited]",
+    rarity: "Mythical Limited",
+    power: 8500,
+    cost: 8,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/ee/72/81/ee7281d6b538979b947dbdf00a04ca58.jpg"
+  },
+  {
+    id: "muzan_final_form",
+    name: "Muzan Kibutsuji [Final Form Limited]",
+    rarity: "Mythical Limited",
+    power: 9000,
+    cost: 10,
+    type: "mythical",
+    image: "https://i.pinimg.com/736x/4a/3b/a8/4a3ba8b84645185055a4469e85c62173.jpg"
+  }
+];
