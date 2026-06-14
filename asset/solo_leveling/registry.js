3998349c5fb3ad8fef843dbcd46021c1532b1944
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-06-14 16:09:24 +0530

    Create registry.js

---
+++ asset/solo_leveling/registry.js
@@ -0,0 +1,99 @@
+const demonSlayerCharacters = require("./assets");
+const demonSlayerMonsters = require("./demons");
+const demonSlayerWeapons = require("./weapons");
+const demonSlayerMythical = require("./mythical");
+const demonSlayerGod = require("./godchar");
+
+
+const soloCharacters = require("./solo_leveling/characters");
+const soloMonsters = require("./solo_leveling/monsters");
+const soloWeapons = require("./solo_leveling/weapons");
+const soloMythical = require("./solo_leveling/mythical");
+
+
+const UNIVERSES = {
+
+"Demon Slayer": {
+
+characters: demonSlayerCharacters,
+monsters: demonSlayerMonsters,
+weapons: demonSlayerWeapons,
+mythical: demonSlayerMythical,
+god: demonSlayerGod
+
+},
+
+
+"Solo Leveling": {
+
+characters: soloCharacters,
+monsters: soloMonsters,
+weapons: soloWeapons,
+mythical: soloMythical,
+god: {}
+
+}
+
+};
+
+
+
+function getUniverse(anime){
+
+return UNIVERSES[anime] || UNIVERSES["Demon Slayer"];
+
+}
+
+
+
+function getCharacters(anime){
+
+const data = getUniverse(anime).characters;
+
+return data.characterRawArray || [];
+
+}
+
+
+
+function getMonsters(anime){
+
+const data = getUniverse(anime).monsters;
+
+return data.monsterRawArray || [];
+
+}
+
+
+
+function getWeapons(anime){
+
+const data = getUniverse(anime).weapons;
+
+return data.weaponRawArray || [];
+
+}
+
+
+
+function getMythical(anime){
+
+const data = getUniverse(anime).mythical;
+
+return data.mythicalRawArray || [];
+
+}
+
+
+
+module.exports = {
+
+UNIVERSES,
+
+getUniverse,
+getCharacters,
+getMonsters,
+getWeapons,
+getMythical
+
+};
