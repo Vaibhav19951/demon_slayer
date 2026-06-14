e8eafd2ed846be3b55e1ea22a787b4c86531d7b0
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-06-14 16:01:20 +0530

    Update characters.js

--- asset/solo_leveling/characters.js
+++ asset/solo_leveling/characters.js
@@ -4,21 +4,21 @@ const characterArray = [
 id:"sung_jinwoo",
 name:"Sung Jin-Woo",
 anime:"Solo Leveling",
-description:"An E-Rank hunter who starts from the bottom.",
-abilities:["Basic Dagger Attack","Survival Instinct"],
+description:"E-Rank hunter who rises from the bottom.",
+abilities:["Dagger Attack","Survival Instinct"],
 rank:"E-Rank Hunter",
 hp:100, atk:20, defense:10, speed:15,
-rarity:"R",
+rarity:"SSR",
 image:"https://i.pinimg.com/736x/cb/df/76/cbdf765a606bb5bdaa398c68d2a7c88e.jpg",
 level:1,xp:0,max_xp:100
 },
 
 {
 id:"yoo_jinho",
-name:"Yoo Jinho",
+name:"Yoo Jin-Ho",
 anime:"Solo Leveling",
-description:"A loyal hunter and Jin-Woo's companion.",
-abilities:["Support","Defense Boost"],
+description:"Loyal E-Rank support hunter.",
+abilities:["Support","Shield Help"],
 rank:"E-Rank Hunter",
 hp:120, atk:15, defense:25, speed:10,
 rarity:"R",
@@ -27,10 +27,49 @@ level:1,xp:0,max_xp:100
 },
 
 {
+id:"han_songyi",
+name:"Han Song-Yi",
+anime:"Solo Leveling",
+description:"Young trainee hunter.",
+abilities:["Growth"],
+rank:"E-Rank Hunter",
+hp:100, atk:18, defense:15, speed:20,
+rarity:"N",
+image:"https://i.pinimg.com/736x/8e/05/c8/8e05c87971188b8ae0d7801b15782d11.jpg",
+level:1,xp:0,max_xp:100
+},
+
+{
+id:"kim_sangshik",
+name:"Kim Sangshik",
+anime:"Solo Leveling",
+description:"Low-tier dungeon hunter.",
+abilities:["Basic Combat"],
+rank:"D-Rank Hunter",
+hp:130, atk:25, defense:20, speed:20,
+rarity:"N",
+image:"URL",
+level:1,xp:0,max_xp:100
+},
+
+{
+id:"park_heejin",
+name:"Park Hee-Jin",
+anime:"Solo Leveling",
+description:"Agile D-Rank hunter.",
+abilities:["Quick Move"],
+rank:"D-Rank Hunter",
+hp:140, atk:30, defense:25, speed:35,
+rarity:"N",
+image:"URL",
+level:1,xp:0,max_xp:100
+},
+
+{
 id:"lee_joohee",
 name:"Lee Joo-Hee",
 anime:"Solo Leveling",
-description:"A healer-type hunter.",
+description:"C-Rank healer hunter.",
 abilities:["Healing","Recovery"],
 rank:"C-Rank Hunter",
 hp:150, atk:20, defense:30, speed:25,
@@ -43,7 +82,7 @@ level:1,xp:0,max_xp:100
 id:"song_chiyul",
 name:"Song Chi-Yul",
 anime:"Solo Leveling",
-description:"A veteran hunter skilled with swords.",
+description:"Veteran C-Rank swordsman.",
 abilities:["Sword Slash"],
 rank:"C-Rank Hunter",
 hp:180, atk:35, defense:35, speed:25,
@@ -53,39 +92,26 @@ level:1,xp:0,max_xp:100
 },
 
 {
-id:"kim_sangshik",
-name:"Kim Sangshik",
+id:"kim_chul",
+name:"Kim Chul",
 anime:"Solo Leveling",
-description:"A normal hunter.",
-abilities:["Basic Combat"],
-rank:"D-Rank Hunter",
-hp:130, atk:25, defense:20, speed:20,
-rarity:"N",
-image:"https://i.pinimg.com/736x/85/af/51/85af5157da568d9b55d59c34b311056a.jpg",
-level:1,xp:0,max_xp:100
-},
-
-{
-id:"park_heejin",
-name:"Park Heejin",
-anime:"Solo Leveling",
-description:"A hunter with survival skills.",
-abilities:["Quick Move"],
-rank:"D-Rank Hunter",
-hp:140, atk:30, defense:25, speed:35,
-rarity:"N",
-image:"https://i.pinimg.com/1200x/93/c5/71/93c5712c9a07273513ebb59f7e38a569.jpg",
+description:"B-Rank fighter hunter.",
+abilities:["Heavy Strike"],
+rank:"B-Rank Hunter",
+hp:250, atk:60, defense:50, speed:40,
+rarity:"SR",
+image:"URL",
 level:1,xp:0,max_xp:100
 },
 
 {
 id:"kang_taeshik",
-name:"Kang Taeshik",
+name:"Kang Tae-Shik",
 anime:"Solo Leveling",
-description:"A skilled assassin hunter.",
+description:"Assassin B-Rank hunter.",
 abilities:["Stealth","Fast Strike"],
 rank:"B-Rank Hunter",
-hp:250, atk:60, defense:40, speed:70,
+hp:260, atk:65, defense:45, speed:75,
 rarity:"SR",
 image:"https://i.pinimg.com/736x/bb/a4/f3/bba4f3bbd2b3333eccd42447e6897bf6.jpg",
 level:1,xp:0,max_xp:100
@@ -93,9 +119,9 @@ level:1,xp:0,max_xp:100
 
 {
 id:"woo_jinchul",
-name:"Woo Jinchul",
+name:"Woo Jin-Chul",
 anime:"Solo Leveling",
-description:"A disciplined hunter association member.",
+description:"Hunter Association inspector.",
 abilities:["Leadership"],
 rank:"A-Rank Hunter",
 hp:300, atk:70, defense:60, speed:50,
@@ -105,30 +131,108 @@ level:1,xp:0,max_xp:100
 },
 
 {
-id:"go_myeonghwan",
-name:"Go Myeonghwan",
+id:"min_byunggu",
+name:"Min Byung-Gyu",
 anime:"Solo Leveling",
-description:"A dungeon hunter.",
-abilities:["Power Strike"],
-rank:"D-Rank Hunter",
-hp:150, atk:30, defense:20, speed:20,
-rarity:"N",
-image:"YOUR_URL",
+description:"S-Rank healer hunter.",
+abilities:["Healing","Support"],
+rank:"S-Rank Hunter",
+hp:300, atk:40, defense:60, speed:50,
+rarity:"SSR",
+image:"https://i.pinimg.com/1200x/bb/b6/1e/bbb61ea3e4e998cf5578b3c3ce1ceb7a.jpg",
 level:1,xp:0,max_xp:100
 },
 
 {
-id:"han_songyi",
-name:"Han Song-Yi",
+id:"baek_yoonho",
+name:"Baek Yoon-Ho",
 anime:"Solo Leveling",
-description:"A young hunter in training.",
-abilities:["Growth"],
-rank:"E-Rank Hunter",
-hp:100, atk:18, defense:15, speed:20,
-rarity:"N",
-image:"https://i.pinimg.com/736x/8e/05/c8/8e05c87971188b8ae0d7801b15782d11.jpg",
+description:"Beast-type S-Rank hunter.",
+abilities:["Beast Transformation"],
+rank:"S-Rank Hunter",
+hp:400, atk:90, defense:80, speed:70,
+rarity:"SSR",
+image:"https://i.pinimg.com/736x/3d/b3/aa/3db3aad19c3a8416bca6f2c9f4ed513e.jpg",
 level:1,xp:0,max_xp:100
-}
+},
+
+{
+id:"cha_haein",
+name:"Cha Hae-In",
+anime:"Solo Leveling",
+description:"Elite S-Rank swordswoman.",
+abilities:["Sword Dance"],
+rank:"S-Rank Hunter",
+hp:420, atk:100, defense:85, speed:90,
+rarity:"SSR",
+image:"https://i.pinimg.com/736x/b4/08/c4/b408c40074761098d91a300637aa0ba4.jpg",
+level:1,xp:0,max_xp:100
+},
+
+{
+id:"go_gunhee",
+name:"Go Gun-Hee",
+anime:"Solo Leveling",
+description:"Hunter Association chairman.",
+abilities:["Authority Aura"],
+rank:"S-Rank Hunter",
+hp:450, atk:110, defense:100, speed:60,
+rarity:"SSR",
+image:"https://i.pinimg.com/736x/e8/6f/94/e86f9468ead127e69d060aa115911774.jpg",
+level:1,xp:0,max_xp:100
+},
+
+{
+id:"choi_jongin",
+name:"Choi Jong-In",
+anime:"Solo Leveling",
+description:"Fire mage S-Rank hunter.",
+abilities:["Fire Magic"],
+rank:"S-Rank Hunter",
+hp:380, atk:120, defense:60, speed:70,
+rarity:"SSR",
+image:"https://i.pinimg.com/736x/59/6b/ef/596bef296bfc11e8c26681bf6d67a198.jpg",
+level:1,xp:0,max_xp:100
+},
+
+{
+id:"lim_taegyu",
+name:"Lim Tae-Gyu",
+anime:"Solo Leveling",
+description:"Elite archer S-Rank hunter.",
+abilities:["Arrow Rain"],
+rank:"S-Rank Hunter",
+hp:360, atk:110, defense:65, speed:95,
+rarity:"SSR",
+image:"https://i.pinimg.com/736x/79/bd/cd/79bdcd476612f2682507444455dedddf.jpg",
+level:1,xp:0,max_xp:100
+},
+
+{
+id:"ma_dongwook",
+name:"Ma Dong-Wook",
+anime:"Solo Leveling",
+description:"Tank S-Rank hunter.",
+abilities:["Iron Body"],
+rank:"S-Rank Hunter",
+hp:500, atk:80, defense:120, speed:50,
+rarity:"SSR",
+image:"https://i.pinimg.com/1200x/66/a6/3c/66a63c162a9f313a7cfaedf1f8991d13.jpg",
+level:1,xp:0,max_xp:100
+},
+
+{
+id:"kim_chul_alt",
+name:"Kim Chul",
+anime:"Solo Leveling",
+description:"B-Rank fighter hunter.",
+abilities:["Heavy Strike"],
+rank:"B-Rank Hunter",
+hp:250, atk:60, defense:50, speed:40,
+rarity:"SR",
+image:"https://i.pinimg.com/736x/96/cb/45/96cb4555c2015ed6f313695cc631f61b.jpg",
+level:1,xp:0,max_xp:100
+},
 
 ];
 
@@ -148,7 +252,6 @@ speed:char.speed,
 abilities:char.abilities
 };
 });
-
 
 module.exports={
 characters,
